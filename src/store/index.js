84582b2e6825633185375e440d12d602b37a40f8
hagleyson <hagleyson@gmail.com>
2022-01-10 00:48:16 -0300

    finished project

--- src/store/index.js
+++ src/store/index.js
@@ -1,32 +1,14 @@
-import { createStore } from "redux";
-import { configureStore, createSlice } from "@reduxjs/toolkit";
-const initialState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter += action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
+import { configureStore } from "@reduxjs/toolkit";
+import counterSlice from "./counter";
+import authSlice from "./auth";
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 export const counterActions = counterSlice.actions;
-
+export const authActions = authSlice.actions;
 export default store;
+
+//import { createStore } from "redux";
 // const counterReducer = (state = initialState, action) => {
 //   if (action.type === "increment") {
 //     return { counter: state.counter + 1, showCounter: state.showCounter };
